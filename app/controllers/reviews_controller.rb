46fa6e66410590b7cf89a2528209e70f7cbd1335
cedric ettori <ettori.cedric@gmail.com>
2021-08-11 17:13:39 +0200

    yelp rails fini

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,26 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:index, :new, :create]
+  def new 
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,42 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+#   require "faker"
+require "faker"
+CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name  ,
+    address: Faker::Address.street_address,
+    category: CATEGORY[rand(0..4)],
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164 
+  )
+  restaurant.save!
+end
+puts 'restaurant Finished!'
+
+5.times do
+  review = Review.new(
+    content: Faker::Restaurant.review,
+    rating: rand(0..5),
+    restaurant_id: 1 
+  )
+  review.save
+end
+puts 'review 1 Finished!'
+5.times do
+  review = Review.new(
+    content: Faker::Restaurant.review,
+    rating: rand(0..5),
+    restaurant_id: rand(1..3)
+  )
+  review.save!
+end
+puts 'review 2 Finished!'
+
+
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
